7903888d540929499349e3a8ad7e1486a0668535
Ernesto Redonet <ushernew1422@gmail.com>
2020-06-04 08:03:57 -0400

    Size container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
-import Jitsi from 'react-jitsi'
- 
-const roomName = 'my-super-secret-meeting-123e4567-e89b-12d3-a456-426655440000'
-const userFullName = 'Ernesto'
- 
- const App = () => (
-    <>
-        <h2>My First Meeting!</h2>
-        <Jitsi roomName={roomName} displayName={userFullName}/>
-    </>
+import React from "react";
+import Jitsi from "react-jitsi";
 
-)
+const roomName = "my-super-secret-meeting-xxx-tCDTJk3v6o3LA";
+const userFullName = "User";
+
+const App = () => (
+  <>
+    <h2>My First Meeting!</h2>
+    <Jitsi
+      roomName={roomName}
+      displayName={userFullName}
+      containerStyle={{ width: "100%", height: "100vh" }}
+    />
+  </>
+);
 
 export default App;
